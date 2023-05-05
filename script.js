14040656c4527959ee9a66f8ae1012e1c351ef91
Karmen <katgucilatar@outlook.com>
2023-05-04 20:49:23 -0400

    specified variables in js and added more to local storage

--- script.js
+++ script.js
@@ -1,21 +1,74 @@
+//display current day and time 
 var currentDay = document.getElementById('currentDay');
-currentDay.textContent = currentDay.format('LLLL');
+currentDay.textContent = dayjs();
+var timeBlock = document.querySelector('.time-block');
+var saveBtn = document.getElementById('save-btn');
 
-var currentHour = currentDay.hours();
+//define 9am-5pm variables to target in dterminePastPresentFuture function
+var nineAm = document.querySelector('#hour-9');
+nineAm = 9;
+var tenAm = document.querySelector('#hour-10')
+tenAm = 10;
+var elevenAm = document.querySelector('#hour-11')
+elevenAm = 11;
+var twelvePm = document.querySelector('#hour-12')
+twelvePm = 12;
+var onePm = document.querySelector('#hour-13')
+onePm = 13;
+var twoPm = document.querySelector('#hour-14')
+twoPm = 14;
+var threePm = document.querySelector('#hour-15')
+threePm = 15;
+var fourPm = document.querySelector('#hour-16')
+fourPm = 16;
+var fivePm = document.querySelector('#hour-17')
+fivePm = 17;
 
-$('.time-block').each(function () {
-    var hour = parseInt($(this).attr('id').split("hour")[1]);
+//create function to create past, present, future
+function determinePastPresentFuture () {
+    var currentHour = dayjs().format("HH");
+    console.log(currentHour);
+    for (var i = 8; i < 18; i++) {
+        var hour = i; 
+        console.log(hour) 
+    }
+        if (hour < currentHour) {
+            $(this).removeClass('time-block')
+            $(this).addClass('past'); 
+        }
+        else if (hour === currentHour) {
+            $(this).removeClass('time-block')
+            $(this).addClass('present');
+    }
+        else {
+            $(this).removeClass('time-block')
+           $(this).addClass('future');
+        }
+    }
+determinePastPresentFuture ()
 
-if (hour < currentHour) {
-    document.querySelector(".row time-block").style.backgroundcolor = "gray"
+
+
+function saveLocalStorage () {
+    var task = document.querySelector('#task')
+    var taskInput = task.textContent 
+    var timeBlocks = document.querySelector('.time-block');
+
+    localStorage.setItem('timeBlocks', JSON.stringify('task'));
+    console.log(localStorage)
 }
-else if (hour === currentHour) {
-    document.querySelector(".row time-block").style.backgroundcolor = "red"
-}
-else {
-    document.querySelector(".row time-block").style.backgroundcolor = "green"
-}
-});
+
+saveBtn.addEventListener('click', saveLocalStorage);
+
+nineAm.value = localStorage.getItem('task')
+tenAm.value = localStorage.getItem('task')
+elevenAm.value = localStorage.getItem('task')
+twelvePm.value = localStorage.getItem('task')
+onePm.value = localStorage.getItem('task')
+twoPm.value = localStorage.getItem('task')
+threePm.value = localStorage.getItem('task')
+fourPm.value = localStorage.getItem('task')
+fivePm.value = localStorage.getItem('task')
 
 
 
