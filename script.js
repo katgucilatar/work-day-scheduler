25b1a9b098cd49552d3460d27ab4bef55e5aa5b0
Karmen <katgucilatar@outlook.com>
2023-05-12 23:03:30 -0400

    fix JavaScript functionality

--- script.js
+++ script.js
@@ -3,72 +3,51 @@ var currentDay = document.getElementById('currentDay');
 currentDay.textContent = dayjs();
 var timeBlock = document.querySelector('.time-block');
 var saveBtn = document.getElementById('save-btn');
+let saveTask = localStorage.getItem("timeBlocks")
 
-//define 9am-5pm variables to target in dterminePastPresentFuture function
-var nineAm = document.querySelector('#hour-9');
-nineAm = 9;
-var tenAm = document.querySelector('#hour-10')
-tenAm = 10;
-var elevenAm = document.querySelector('#hour-11')
-elevenAm = 11;
-var twelvePm = document.querySelector('#hour-12')
-twelvePm = 12;
-var onePm = document.querySelector('#hour-13')
-onePm = 13;
-var twoPm = document.querySelector('#hour-14')
-twoPm = 14;
-var threePm = document.querySelector('#hour-15')
-threePm = 15;
-var fourPm = document.querySelector('#hour-16')
-fourPm = 16;
-var fivePm = document.querySelector('#hour-17')
-fivePm = 17;
 
 //create function to create past, present, future
-function determinePastPresentFuture () {
+
+$(".time-block").each(function () {
     var currentHour = dayjs().format("HH");
-    console.log(currentHour);
-    for (var i = 8; i < 18; i++) {
-        var hour = i; 
-        console.log(hour) 
+    let hour = $(this).attr("id").split("-")
+    console.log(hour)
+    if (hour[1] < currentHour) {
+        $(this).addClass('past');
     }
-        if (hour < currentHour) {
-            $(this).removeClass('time-block')
-            $(this).addClass('past'); 
-        }
-        else if (hour === currentHour) {
-            $(this).removeClass('time-block')
-            $(this).addClass('present');
+    else if (hour[1] === currentHour) {
+        $(this).addClass('present');
     }
-        else {
-            $(this).removeClass('time-block')
-           $(this).addClass('future');
-        }
+    else {
+        $(this).addClass('future');
     }
-determinePastPresentFuture ()
+})
 
 
-
-function saveLocalStorage () {
-    var task = document.querySelector('#task')
-    var taskInput = task.textContent 
-    var timeBlocks = document.querySelector('.time-block');
-
-    localStorage.setItem('timeBlocks', JSON.stringify('task'));
+// create function to save items in local storage
+function saveLocalStorage(event) {
+    let id = event.target.parentNode.id
+    var taskInput = event.target.previousElementSibling.value
+    console.log(taskInput)
+    localStorage.setItem('timeBlocks', JSON.stringify({ time: id, task: taskInput }));
     console.log(localStorage)
 }
 
 saveBtn.addEventListener('click', saveLocalStorage);
 
-nineAm.value = localStorage.getItem('task')
-tenAm.value = localStorage.getItem('task')
-elevenAm.value = localStorage.getItem('task')
-twelvePm.value = localStorage.getItem('task')
-onePm.value = localStorage.getItem('task')
-twoPm.value = localStorage.getItem('task')
-threePm.value = localStorage.getItem('task')
-fourPm.value = localStorage.getItem('task')
-fivePm.value = localStorage.getItem('task')
+// create function to get items from local storage
+$(".description").each(function () {
+    let time = $(this).parent().attr("id")
+    let parseTasks = JSON.parse(saveTask)
+    for (let index = 0; index < parseTasks.length; index++) {
+        const element = parseTasks[index];
+        console.log(element)
+        if (time == element.time) {
+            $(this).val(element.task)
+        }
+    }
+}
+)
 
 
 
